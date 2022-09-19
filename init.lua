87752ea9c977434db29615b5a7ccae73ec480bb6
Markus Pröpper <markus.proepper@t-online.de>
2022-09-19 14:01:41 +0200

    Installed Plugin Telescope

--- init.lua
+++ init.lua
@@ -18,6 +18,8 @@ require('packer').startup(function()
     use 'bluz71/vim-nightfly-guicolors'
     use 'nvim-lualine/lualine.nvim'
     use 'kyazdani42/nvim-web-devicons'
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-lua/plenary.nvim'
 end)
 
 -- [[ COLORSCHEMA ]]
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,6 +89,16 @@ _G.packer_plugins = {
     path = "/Users/mpro/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/Users/mpro/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/Users/mpro/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
   ["vim-nightfly-guicolors"] = {
     loaded = true,
     path = "/Users/mpro/.local/share/nvim/site/pack/packer/start/vim-nightfly-guicolors",
